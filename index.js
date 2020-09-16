65eb4dc9d194872a48171bab1f2d95792aaca9f5
Jasmine Ou <jasmineou99@gmail.com>
2020-09-16 18:42:36 -0400

    Clean up socket logic.

--- index.js
+++ index.js
@@ -13,15 +13,22 @@ const io = require('socket.io')(httpServer);
 io.on('connect', socket => {
   console.log('connect');
 
-  // Notify all other connected clients that a new user joined.
+  // Receive a signal that a new user joined.
   socket.on('newUserJoined', data => {
+      // Notify all other users that a new user joined.
       socket.broadcast.emit('newUserJoined', data);
 
-      // Notify the new user of an existing user that wants to greet it.
       socket.on('greetNewUser', data => {
+          // Notify the new user of an existing user that wants to greet it.
           socket.emit('greetNewUser', data);
       }); 
   });
+
+  // Receive signal that a user wants to update all users on a change.
+  socket.on('updateAllUsers', data => {
+    // Notify all other users that an existing user made a change.
+    socket.broadcast.emit('updateUser', data);
+  })
  
 });
 
